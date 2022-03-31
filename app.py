3afdf4c9038c64865782955426f8f73ecb5aff57
Akhil Rao <82179056+Akhil1438@users.noreply.github.com>
2022-03-30 22:02:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request,render_template,return
 
 app = Flask(__name__)
 app.config['DATABASE'] = 'db.sqlite3'
@@ -9,6 +9,9 @@ db.init_app(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+def Checklogin():
+    EID = request.form['Email ID']
 
 if __name__ == '__main__':
     app.run(debug=True)
