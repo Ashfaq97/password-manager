9a5b71f7a8d27bcddc42ea7522456a39d975cfd3
Jamison Ball <jaball1993@aol.com>
2022-03-30 18:47:40 -0700

    Added code to create database

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['DATABASE'] = 'db.sqlite3'
+
+import db
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
