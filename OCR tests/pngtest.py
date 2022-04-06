f9ddc4dae6a625a6bf50285ad8f39c941e7b519a
Jamison Ball <jaball1993@aol.com>
2022-04-05 19:13:25 -0700

    Create template folder if misspelled just rename and commit to the repo

--- OCR tests/pngtest.py
+++ OCR tests/pngtest.py
@@ -1,5 +1,17 @@
-from PIL import Image
 import pytesseract
+from spellchecker import SpellChecker
+
+def spellcheck(words):
+    spellchecker = SpellChecker()
+
+    misspelled = spellchecker.unknown(words)
+    corrected_words = []
+
+    for word in words:
+        if word in misspelled:
+           corrected_words.append(spellchecker.correction(word))
+    
+    return " ".join(corrected_words)
 
 print('Results from OCR:\n\n')
-print(pytesseract.image_to_string('test.png'))
+print(pytesseract.image_to_string('test1.jpeg'))
